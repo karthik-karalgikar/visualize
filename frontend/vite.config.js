4e5eba8495ba1b44188322a8df7dc4f008ca6a4a
vallabhvy <113258129+vallabhvy@users.noreply.github.com>
2026-01-11 01:14:11 +0530

    long vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,31 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  ssr: {
-    noExternal: ["react-remove-scroll"],
+
+  optimizeDeps: {
+    include: ["react", "react-dom", "@mantine/core", "@mantine/hooks"],
+  },
+
+  build: {
+    chunkSizeWarningLimit: 1000,
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom"],
+          "mantine-vendor": ["@mantine/core", "@mantine/hooks"],
+        },
+      },
+    },
+
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true,
+    },
+  },
+
+  resolve: {
+    dedupe: ["react", "react-dom"],
+    conditions: ["import", "module", "browser", "default"],
   },
 });
