18c41be81034268118dc28af01ce9aa88b7fd1eb
vallabhvy <113258129+vallabhvy@users.noreply.github.com>
2026-01-11 00:43:58 +0530

    new build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "react-remove-scroll": "react-remove-scroll/dist/es2015/index.js",
-    },
-  },
-
-  optimizeDeps: {
-    include: ["react-remove-scroll"],
-  },
 });
