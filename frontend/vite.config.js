225a2b173b110c4666338e3470800038f8b088b2
vallabhvy <113258129+vallabhvy@users.noreply.github.com>
2026-01-27 23:54:16 +0530

    i cant anymore

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  resolve: {
-    dedupe: ["react", "react-dom"],
-  },
+  plugins: [react()],
 });
