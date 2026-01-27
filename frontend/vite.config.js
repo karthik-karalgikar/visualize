6aacacceacbb2ea2663fa88336b7936d27d4295d
vallabhvy <113258129+vallabhvy@users.noreply.github.com>
2026-01-27 23:59:49 +0530

    kill me

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  optimizeDeps: {
+    include: ['@monaco-editor/react', 'monaco-editor']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/@monaco-editor/, /node_modules/]
+    }
+  }
+})
